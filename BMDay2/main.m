e32a19566311a93df9fd1dd650646f210c599aba
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-07 11:12:25 -0400

    Added Basic Variables - Int, Float, String, Array

--- BMDay2/main.m
+++ BMDay2/main.m
@@ -11,7 +11,22 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //declared int variable
+        int productNumber = 100;
+        //declared floating point value
+        float productWeight = 105.67;
+        //declared strings
+        NSString *recipeName = @"Lasagne";
+        NSString *recipeName2 = @"Cat Food";
+        
+        NSArray *recipeNames = @[@"Toast", @"Bacon", @"Eggs", recipeName, recipeName2];
+        
+        
+        
+        NSLog(@"My favourite food is %@ and I could eat it about a %i times or %f times to be exact", recipeName, productNumber, productWeight);
+        NSLog(@"%@",recipeNames);
+        //NSLog(@"Hello, World!");
+        
     }
     return 0;
 }
